13788e5c80e0428b08881f9765889a846d518eab
DESKTOP-NFG3LQ4\Admin <mtamkg93@gmail.com>
2022-07-09 21:14:38 +0700

    remove mat module

--- src/app/shared/components/data-table/data-table.module.ts
+++ src/app/shared/components/data-table/data-table.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import { MaterialModule } from '@app/app.module';
 import { PaginatorModule } from '../paginator/paginator.module';
 import { DataTableComponent } from './data-table.component';
 
 @NgModule({
   declarations: [DataTableComponent],
-  imports: [CommonModule, MaterialModule, MatTableModule, PaginatorModule],
+  imports: [CommonModule, MatTableModule, PaginatorModule],
   exports: [DataTableComponent],
 })
 export class DataTableModule {}

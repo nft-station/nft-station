89577122f44836b5f9d362eaded55bd7f9e9e572
DESKTOP-NFG3LQ4\Admin <mtamkg93@gmail.com>
2022-07-20 00:36:45 +0700

    update transfer token

--- src/app/pages/transfer/transfer-token/transfer-token.component.ts
+++ src/app/pages/transfer/transfer-token/transfer-token.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { BTN_COLOR_GRADIENT } from '@app/core/constants/common.constant';
+import { ContractService } from '@app/core/services/contract.service';
+import { WalletService } from '@app/core/services/wallet.service';
 
 @Component({
   selector: 'app-transfer-token',
@@ -8,4 +10,36 @@ import { BTN_COLOR_GRADIENT } from '@app/core/constants/common.constant';
 })
 export class TransferTokenComponent {
   btnColor = BTN_COLOR_GRADIENT;
+  address = '';
+  addressTransfer = '';
+  invalidName = false;
+  constructor(private contractService: ContractService, private walletService: WalletService) {}
+
+  checkWallet() {
+    this.invalidName = false;
+    if (this.address.length > 0 && !(this.address.startsWith('aura') && this.address.length > 20)) {
+      this.contractService
+        .queryContractSmart({
+          address_of: {
+            token_id: this.address,
+          },
+        })
+        .subscribe({
+          next: e => {
+            console.log(e);
+            this.invalidName = false;
+          },
+          error: e => {
+            this.invalidName = true;
+            console.log(e);
+          },
+          complete: () => {
+            this.invalidName = false;
+          },
+        });
+    }
+  }
+
+  transferToken() {
+  }
 }
